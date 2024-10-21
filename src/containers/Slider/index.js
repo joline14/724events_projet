57728b50846eb296feea8fb134f72e9da9dc743b
Joline Sinci <jolinesinci14@gmail.com>
2024-10-21 16:10:10 +0200

    Réparation du bug d'img blanche dans le slider

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -10,19 +10,23 @@ const Slider = () => {
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
     new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
   );
-  const nextCard = () => {
-    setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
-      5000
-    );
-  };
+  
   useEffect(() => {
-    nextCard();
-  });
-
+    // Timer pour changer d'index toutes les 5s
+    const timer = setTimeout(() => {
+     // Incrémente l'index ou le remet à 0 si la fin est atteinte
+     setIndex((prevIndex) => (prevIndex < (byDateDesc?.length ?? 0) - 1 ? prevIndex + 1 : 0));
+    }, 5000);
+    // Nettoyage du timer lorsque le composant est démonté ou que l'index change
+    return () => clearTimeout(timer);
+     // Effet relancé quand `index` ou `byDateDesc` change
+  }, [index, byDateDesc]);
+  
   // Gestion du changement via les boutons radio
   const handleRadioChange = (radioIdx) => {
     setIndex(radioIdx);
+      // Annulation du timer de transition automatique
+      clearTimeout();
   };
   
   return (
